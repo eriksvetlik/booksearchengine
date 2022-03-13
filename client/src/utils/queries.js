2dafce0206bf30a86ad63e865a42a21517fce7d7
eriksvetlik <svetlik.erik@gmail.com>
2022-03-13 13:08:01 -0500

    updated mutations and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+export const QUERY_ME = gql`
+  query me {
+    me {
       _id
-      name
-    }
-  }
-`;
-
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
