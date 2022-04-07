ac84dfe3959cef1bd765dce9e58e4e414bdc6de3
eriksvetlik <svetlik.erik@gmail.com>
2022-04-07 15:22:46 -0500

    updated SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
@@ -16,7 +16,15 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser] = useMutation(ADD_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -31,15 +39,30 @@ const SignupForm = () => {
     event.preventDefault();
     console.log(userFormData);
 
+    // check if form has everything
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
     try {
       const { data } = await addUser({
         variables: { ...userFormData },
       });
 
+      console.log(data);
       Auth.login(data.addUser.token);
     } catch (error) {
       console.error(error);
     }
+
+    // clear form values
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
   };
 
   return (
