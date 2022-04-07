4fafa47ca0e1f1f54226b31964c2ff5f82e0c133
eriksvetlik <svetlik.erik@gmail.com>
2022-04-07 15:19:13 -0500

    updated LoginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,15 +1,23 @@
-// see SignupForm.js for comments
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
+
 import Auth from "../utils/auth";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [loginUser] = useMutation(LOGIN_USER);
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -19,11 +27,18 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
     try {
       const { data } = await loginUser({
         variables: { ...userFormData },
       });
 
+      console.log(data);
       Auth.login(data.login.token);
     } catch (error) {
       console.error(error);
@@ -31,7 +46,6 @@ const LoginForm = () => {
 
     // clear form values
     setUserFormData({
-      username: "",
       email: "",
       password: "",
     });
